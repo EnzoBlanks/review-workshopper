002756eba3c56ba39eff7b8186179722a1d1cbfb
Enzo Blanks <enzoblanks@BetamoreAdmins-MacBook-Air-3.local>
2022-03-10 14:42:45 -0500

    Array of Sunshine

---
+++ 4-arrays.js
@@ -0,0 +1,13 @@
+const fruits = ["Grapes", "Bannana", "kiwi", "Lime", "Grapefruit", "Avacado"]
+
+let length = fruits.length;
+
+console.log(fruits[3])
+
+console.log(fruits.replace ("Bannana", "Blueberry"));
+
+let firstFruit = Avacado
+
+
+
+
