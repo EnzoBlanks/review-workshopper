af7bdba74ca0e40a278b08b574c21e2e25825065
Enzo Blanks <enzoblanks@BetamoreAdmins-MacBook-Air-3.local>
2022-03-11 11:33:54 -0500

    Very Able

---
+++ 2-variables.js
@@ -0,0 +1,18 @@
+//let Dave = Mauve;
+
+//let Sarah = Blue;
+
+//let Ernesto = Green;
+
+let Ernesto = "Ernesto's favorite color is Green ";
+
+let Sarah = "Sharah's favorite color is Blue "; 
+
+let Dave = "Dave's favorite color is Mauve";
+
+let firstNames = Ernesto + Sarah + Dave;
+console.log(firstNames);
+
+console.log("Ernesto's favorite color is actually Mauve Sarah loves Green and Dave is a fan of Blue")
+//console.log(Sarah)
+//console.log(Dave)
